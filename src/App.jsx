46913fc63f96603d4044bc0564cb3bd609c1ccbd
KirillMomotenko <kirill_momotenko@mail.ru>
2024-12-23 10:34:33 +0300

    6

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import TodoPage from './pages/TodoPage';
-import DndPage from './pages/DndPage';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import TodoPage from "./pages/TodoPage";
+import DndPage from "./pages/DndPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<TodoPage />} />
-        <Route path="/dnd" element={<DndPage />} />
+        <Route path="/Lab3-4" element={<TodoPage />} />
+        <Route path="/Lab3-4/dnd" element={<DndPage />} />
       </Routes>
     </Router>
   );
